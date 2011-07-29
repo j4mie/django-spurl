72fe53d0a3c2bf77cda2ad6ea8043c584514a57b
Jamie Matthews <jamie.matthews@gmail.com>
2011-07-29 09:39:47 +0100

    Prepare setup.py and MANIFEST for PyPI release

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
+import os
+import re
 from setuptools import setup, find_packages
 
 description = 'A Django template library for manipulating URLs.'
 
+rel_file = lambda *args: os.path.join(os.path.dirname(os.path.abspath(__file__)), *args)
+
+def read_from(filename):
+    fp = open(filename)
+    try:
+        return fp.read()
+    finally:
+        fp.close()
+
+def get_version():
+    data = read_from(rel_file('spurl', '__init__.py'))
+    return re.search(r"__version__ = '([^']+)'", data).group(1)
+
 setup(
     name='django-spurl',
-    version='0.1',
+    version=get_version(),
     description=description,
     long_description=description,
     author='Jamie Matthews',
@@ -12,4 +27,12 @@ setup(
     url='http://github.com/j4mie/django-spurl/',
     packages=find_packages(),
     install_requires=['urlobject==0.5.1'],
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Framework :: Django',
+        'Operating System :: OS Independent',
+    ]
 )
