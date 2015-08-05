b54be4f1040b881c090301b71e466b4b3c947bed
Basil Shubin <basil.shubin@gmail.com>
2015-08-05 10:37:51 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'six',
     ],
     classifiers = [
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
