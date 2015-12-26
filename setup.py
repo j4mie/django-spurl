f520b7320230434fa20ef97bdc0405c7f85ebfb2
bashu <basil.shubin@gmail.com>
2015-12-26 10:13:10 +0500

    updated requirements

--- setup.py
+++ setup.py
@@ -25,11 +25,11 @@ setup(
     license='Public Domain',
 
     install_requires=[
-        'urlobject>=2.0.0',
+        'urlobject>=2.4.0',
         'six',
     ],
     setup_requires=[
-        'urlobject>=2.0.0',
+        'urlobject>=2.4.0',
         'django>=1.4',
         'nose',
         'six',
